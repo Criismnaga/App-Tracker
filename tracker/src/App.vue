68cf9a973703e086f309eff13277e1a43d3d1687
cm@cesar.org.br <cm@cesar.org.br>
2024-06-11 18:41:58 -0300

    Componentes LateralBar e MyForm criados e importados no app. (aula 5)

--- tracker/src/App.vue
+++ tracker/src/App.vue
@@ -1,11 +1,24 @@
-<template></template>
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <LateralBar />
+    </div>
+    <div class="column is-three-quarters">
+      <MyForm> </MyForm>
+    </div>
+  </main>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import LateralBar from "./components/LateralBar.vue";
+import MyForm from "./components/MyForm.vue";
 export default defineComponent({
   name: "App",
-  components: {},
+  components: {
+    LateralBar,
+    MyForm,
+  },
 });
 </script>
 
